cee0cd16862c518a4e18701eff0446a66ca9354b
Rajan Mali <mysteriouskiller50@gmail.com>
2020-11-21 21:08:41 +0545

    react form hooks setup

--- client/src/components/LogEntryForm.js
+++ client/src/components/LogEntryForm.js
@@ -1,18 +1,32 @@
+import { useForm } from 'react-hook-form';
+
 const LogEntryForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <>
       <h3>Add your log entry here</h3>
-      <form className="entry-form">
+      <form onSubmit={handleSubmit(onSubmit)} className="entry-form">
         <label htmlFor="title">Title:&nbsp;</label>
-        <input type="text" name="title" />
+        <input type="text" name="title" ref={register} required aria-required />
         <label htmlFor="comments">Comments:&nbsp;</label>
-        <textarea name="comments" rows={3} />
+        <textarea name="comments" rows={3} ref={register} />
         <label htmlFor="description">Description:&nbsp;</label>
-        <textarea name="description" rows={3} />
+        <textarea name="description" rows={3} ref={register} />
         <label htmlFor="image">Image:&nbsp;</label>
-        <input type="file" name="image" />
+        <input type="file" name="image" ref={register} />
         <label htmlFor="visitDate">Visit Date:&nbsp;</label>
-        <input type="date" name="visitDate" />
+        <input
+          type="date"
+          name="visitDate"
+          required
+          aria-required
+          ref={register}
+        />
+        <button>Register Travel Log</button>
       </form>
     </>
   );
