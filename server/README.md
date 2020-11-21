a957f7deada51a841c9480f9a31c8e3c53331e18
Rajan Mali <mysteriouskiller50@gmail.com>
2020-11-21 15:50:11 +0545

    mongo schema setup and env setup

---
+++ server/README.md
@@ -0,0 +1,13 @@
+# LOG ENTRY
+
+- Title - Text
+- Description - Text
+- Comments - Text
+- Rating - scale of 1 - 10
+- Images - Text - URL
+- Start Date - DateTime
+- End Date - DateTime
+- Longitude - Number
+- Latitude - Number
+- Created At - DateTime
+- Updated At - DateTime
--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
 
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
   })
 );
 
@@ -16,21 +25,9 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
-app.use((req, res, next) => {
-  const error = new Error(`Cannot find the route - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
+app.use(middlewares.notFound);
 
-// eslint-disable-next-line no-unused-vars
-app.use((error, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
-  });
-});
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
 
