8375a5787ddbd5224c684cf114e0376c1d68325f
Rajan Mali <mysteriouskiller50@gmail.com>
2020-11-21 16:17:32 +0545

    body parser middleware and route setup

---
+++ server/api/logs.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const LogEntry = require('../models/LogEntry');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: '🌏' });
+});
+
+router.post('/', (req, res) => {
+  const logEntry = new LogEntry(req.body);
+  console.log(req.body);
+});
+
+module.exports = router;
--- server/src/index.js
+++ server/src/index.js
@@ -7,9 +7,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const middlewares = require('./middlewares');
+const logs = require('../api/logs');
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const app = express();
@@ -20,10 +22,13 @@ app.use(
     origin: process.env.CORS_ORIGIN,
   })
 );
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 });
+
+app.use('/api/logs', logs);
 
 app.use(middlewares.notFound);
 
