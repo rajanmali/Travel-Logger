be5c7eda58e246c174421a7b381c0671df836130
Rajan Mali <mysteriouskiller50@gmail.com>
2020-11-21 15:09:26 +0545

    error handling middle ware

--- server/src/index.js
+++ server/src/index.js
@@ -12,6 +12,26 @@ app.use(
   })
 );
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Cannot find the route - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+  });
+});
+
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => {
