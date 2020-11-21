ef03f6aa455c103e6cc5a06f39ab9bc588a1408a
Rajan Mali <mysteriouskiller50@gmail.com>
2020-11-21 14:59:52 +0545

    helmet and cors middleware setup complete

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-const port = process.env.PORT || 1337;
-
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
